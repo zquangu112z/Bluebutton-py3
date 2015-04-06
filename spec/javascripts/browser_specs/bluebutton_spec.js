bd71c7440c197612d9a0739388bde0c0508d6ab3
Taeber Rapczak <taeber@ufl.edu>
2015-04-06 11:08:08 -0400

    Remove JavaScript version to replace with Python

--- spec/javascripts/browser_specs/bluebutton_spec.js
+++
@@ -1,9 +0,0 @@
-describe("BlueButton", function() {
-  
-  it("should be defined on window", function() {
-    expect(window.BlueButton).toBeDefined();
-  });
-
-  runDateParsingTests(Documents);
-  
-});
