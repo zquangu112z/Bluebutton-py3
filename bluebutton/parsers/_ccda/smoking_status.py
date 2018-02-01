f3ffe82a090c86ae9eaba23be84551eff3882011
Ngữ Quang Trương <zquangu112z@gmail.com>
2018-02-01 11:03:21 +0700

    feat(parser): parse all topics in social history section, not only smoking status

--- bluebutton/parsers/_ccda/smoking_status.py
+++ bluebutton/parsers/_ccda/smoking_status.py
@@ -12,36 +12,32 @@ from ...core import wrappers, ccda_enum
 from ... import documents
 
 
+# @TODO: rename this method to social_history
 def smoking_status(ccda):
     name = None
     code = None
     code_system = None
     code_system_name = None
 
-    # We can parse all of the social_history sections
-    # but in practice, this section seems to be used for
-    # smoking status, so we're just going to break that out.
     data = wrappers.ListWrapper()
     social_history = ccda.section('social_history')
     entries = social_history.entries()
     for i, entry in ccda_enum(entries, ccda):
 
-        smoking_status_ = entry.template('2.16.840.1.113883.10.20.22.4.78')
-        if smoking_status_.is_empty():
-            smoking_status_ = entry.template('2.16.840.1.113883.10.22.4.78')
+        observation = entry.tag("observation")
 
-        if smoking_status_.is_empty():
-            continue
-
-        el = smoking_status_.tag('effectiveTime')
+        el = observation.tag('effectiveTime')
         start_date, end_date = documents.parse_effectiveTime(el)
 
-        el = smoking_status_.tag('value')
+        el = observation.tag('code')
         source_line = el._element.sourceline
         name = el.attr('displayName')
         code = el.attr('code')
         code_system = el.attr('codeSystem')
         code_system_name = el.attr('codeSystemName')
+
+        el = observation.tag('value')
+        value = el.attr('displayName')
 
         data.append(wrappers.ObjectWrapper(
             section_title=social_history.tag('title')._element.text,
@@ -54,7 +50,9 @@ def smoking_status(ccda):
             name=name,
             code=code,
             code_system=code_system,
-            code_system_name=code_system_name
+            code_system_name=code_system_name,
+            value=value,
+            unit=None
         ))
 
     return data
